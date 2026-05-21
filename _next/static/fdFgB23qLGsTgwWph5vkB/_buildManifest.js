41cffee42e3f5d483f5989242ce6001b24bc9e00
Shahar <shaharh121212@gmail.com>
2026-05-21 22:16:37 +0300

    Fix

---
+++ _next/static/fdFgB23qLGsTgwWph5vkB/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Ptakim//_next/:path+",
+        "destination": "/Ptakim/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
